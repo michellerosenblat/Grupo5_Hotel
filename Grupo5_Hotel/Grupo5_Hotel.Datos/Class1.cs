4adfc14adab017f9a7c2d9ef4eff49602c726693
michellerosenblat <70615029+michellerosenblat@users.noreply.github.com>
2020-10-31 11:39:33 -0300

    Se arma el esquema.

---
+++ Grupo5_Hotel/Grupo5_Hotel.Datos/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo5_Hotel.Datos
+{
+    public class Class1
+    {
+    }
+}
---
+++ Grupo5_Hotel/Grupo5_Hotel.Entidades/Entidades/Persona.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo5_Hotel.Entidades
+{
+    public abstract class Persona
+    {
+        protected int id;
+        protected string nombre;
+        protected string apellido;
+        protected string direccion;
+        protected long telefono;
+        protected string mail;
+
+        public Persona(int id, string nombre, string apellido, string direccion, long telefono, string mail)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.direccion = direccion;
+            this.telefono = telefono;
+            this.mail = mail;
+        }
+
+        public string Nombre { 
+            get 
+            {
+                 return this.nombre;
+            }
+            set
+            {
+                this.nombre = value;
+            }
+            }
+    }
+}
