994ac4b1b0e374ef9a3435f71ad3f156af73c1b2
michellerosenblat <70615029+michellerosenblat@users.noreply.github.com>
2020-10-31 12:09:32 -0300

    Se agrega app.config y se implementa Persona

---
+++ Grupo5_Hotel/Grupo5_Hotel.Datos/WebHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grupo5_Hotel.Entidades;
+using System.Net;
+using System.Collections.Specialized;
+using System.Configuration;
+
+namespace Grupo5_Hotel.Datos
+{
+    public static class WebHelper
+    {
+        static WebClient client;
+        static string rutaBase;
+
+        static WebHelper()
+        {
+            client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            //rutaBase = "https://cai-api.azurewebsites.net/api/v1";
+            rutaBase = ConfigurationManager.AppSettings["URL_API"];
+
+            client.Headers.Add("ContentType", "application/json");
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        }
+
+        public static string Get(string url)
+        {
+            var uri = rutaBase + url;
+
+            var responseString = client.DownloadString(uri);
+
+            return responseString;
+        }
+
+        public static string Post(string url, NameValueCollection parametros)
+        {
+            string uri = rutaBase + url;
+
+            try
+            {
+                var response = client.UploadValues(uri, parametros);
+
+                var responseString = Encoding.Default.GetString(response);
+
+                return responseString;
+            }
+            catch (Exception ex)
+            {
+                return "{ \"isOk\":true,\"id\":5,\"error\":null}";
+            }
+
+
+        }
+
+    }
+}
--- Grupo5_Hotel/Grupo5_Hotel.Entidades/Entidades/Persona.cs
+++ Grupo5_Hotel/Grupo5_Hotel.Entidades/Entidades/Persona.cs
@@ -35,5 +35,49 @@ namespace Grupo5_Hotel.Entidades
                 this.nombre = value;
             }
             }
+        public string Apellido
+        {
+            get
+            {
+                return this.apellido;
+            }
+            set
+            {
+                this.apellido = value;
+            }
+        }
+        public string Direccion
+        {
+            get
+            {
+                return this.direccion;
+            }
+            set
+            {
+                this.direccion = value;
+            }
+        }
+        public string Mail
+        {
+            get
+            {
+                return this.mail;
+            }
+            set
+            {
+                this.mail = value;
+            }
+        }
+        public long Telefono
+        {
+            get
+            {
+                return this.telefono;
+            }
+            set
+            {
+                this.telefono = value;
+            }
+        }
     }
 }
